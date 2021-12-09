8359943a2e284ec980fef7d5ce98734e12e2a557
shashank1503-cipher <54381338+shashank1503-cipher@users.noreply.github.com>
2021-12-09 21:23:04 +0530

    fixed heading

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div className="head"><h1></h1></div>
+      <div className="head"><p></p></div>
       <p className="content">Coming Soon!</p>
     </div>
   );
