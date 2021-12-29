16c2febd9dc69ced43b82a4bd1e56120dfeb0f1b
Shashank Srivastava <54381338+shashank1503-cipher@users.noreply.github.com>
2021-12-29 18:44:08 +0530

    Setup Proxy URL

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -22,7 +22,7 @@ export const AuthProvider = ({ children }) => {
 
   let loginUser = async (e) => {
     e.preventDefault();
-    let response = await fetch("https://partners-in-crime.herokuapp.com/authapi/token/", {
+    let response = await fetch("/authapi/token/", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -52,7 +52,7 @@ export const AuthProvider = ({ children }) => {
     history.push('/')
   };
   let updateToken = async () => {
-    let response = await fetch("https://partners-in-crime.herokuapp.com/authapi/token/refresh/", {
+    let response = await fetch("/authapi/token/refresh/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
