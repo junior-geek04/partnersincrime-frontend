0a336e79dc236779a63334b0a30c2080ff9e8698
divyansh <divyanshpanwar895@gmail.com>
2022-01-05 17:20:08 +0530

    done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Signup from "./pages/Signup/Signup";
 import Login from "./pages/Login/Login";
 import Home from "./pages/Home/Home";
+
 import PrivateRoute from "./utils/PrivateRoute";
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
@@ -14,7 +15,9 @@ function App() {
           <PrivateRoute component={Home} path="/home" />
           <Route component={Login} path="/" exact />
           <Route component={Signup} path="/signup" />
+          
         </AuthProvider>
+        
       </Router>
     </div>
   );
